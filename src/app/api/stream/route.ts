4b5c8bb5c5872302c465e5bece32a04a4414907e
Yin Shanyang <me@yinshanyang.com>
2023-10-02 17:26:48 +0800

    feature(api/stream): 15mb payload

--- src/app/api/stream/route.ts
+++ src/app/api/stream/route.ts
@@ -1,7 +1,7 @@
 import { randomBytes } from 'crypto'
 
 export const GET = async () => {
-  const buffer = randomBytes(24 * 1024 * 1024)
+  const buffer = randomBytes(15 * 1024 * 1024)
   const blob = new Blob([buffer])
   const stream = blob.stream()
 
